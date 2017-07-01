e0ce052fec8cc7c5caabdf2f147a6d068e361e74
VladoGoIt <dzhuhalyk@gmail.com>
2017-07-01 18:24:55 +0300

    зробив варіант 1

--- src/modul2/HomeWork2.java
+++ src/modul2/HomeWork2.java
@@ -1,7 +1,32 @@
 package modul2;
 
+import java.util.Scanner;
+
 /**
  * Created by Volodymyr Dzhuhalyk on 01.07.2017.
  */
 public class HomeWork2 {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Ваше имя: ");
+        String name = in.nextLine();
+
+        System.out.print("Ваш город: ");
+        String city  = in.nextLine();
+
+        System.out.print("Ваш возраст: ");
+        int age = in.nextInt();
+        in.nextLine();
+
+        System.out.print("Ваше хобби: ");
+        String hobby = in.nextLine();
+
+        System.out.println("Имя: " + name);
+        System.out.println("Город: " + city);
+        System.out.println("Возраст: " + age);
+        System.out.println("Хобби: " + hobby);
+
+    }
+
 }
